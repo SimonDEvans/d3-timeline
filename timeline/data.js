74ce27b6ef8eba6b823a757f2c105d8afa7b5365
Simon Evans <simon.evans@torchbox.com>
2017-11-19 14:58:58 +0000

    Add some data

---
+++ timeline/data.js
@@ -0,0 +1,25 @@
+var json = [
+  {
+    "name":"Lane 1",
+    "data":[
+      {"date": "1985-04-08T00:00:00", "details": {"event": "Lorem ipsum", "object": "Dolor sit amet"}}
+    ]
+  },
+  {
+    "name":"Lane 2",
+    "data":[
+      {"date": "1990-04-21T00:00:00", "details": {"event": "Lorem ipsum", "object": "Dolor sit amet"}},
+      {"date": "1992-04-16T00:00:00", "details": {"event": "<a href=''>Link example</a>", "object": "Dipsum sipsum"}},
+      {"date": "1995-04-30T00:00:00", "details": {"event": "Lorem ipsum", "object": "Dolor sit amet"}},
+      {"date": "1998-04-30T00:00:00", "details": {"event": "vmPowerUp", "object": "vmNorm"}}
+    ]
+  },
+  {
+    "name":"Lane 3",
+    "data":[
+      {"date": "1987-04-13T00:00:00", "details": {"event": "Lorem ipsum", "object": "Dolor sit amet"}},
+      {"date": "1991-04-13T00:40:00", "details": {"event": "Lorem ipsum", "object": "Dolor sit amet"}},
+      {"date": "1999-04-12T00:10:00", "details": {"event": "Lorem ipsum", "object": "Dolor sit amet"}}
+    ]
+  }
+]
